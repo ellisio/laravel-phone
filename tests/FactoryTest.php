c3878dca0762e920e8590a1764174f22e54c754f
Andrew Ellis <ellisio@users.noreply.github.com>
2017-09-28 14:46:47 -0600

    100% Coverage (#11)
    * Phone is now 100% covered
    
    * PhoneFactory now has 100% coverage
    
    * PhoneValidator now has 100% coverage

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -2,6 +2,7 @@
 
 namespace EllisIO\Tests\Phone;
 
+use InvalidArgumentException;
 use EllisIO\Phone\PhoneFactory;
 use EllisIO\Phone\Contracts\Driver;
 
@@ -28,6 +29,38 @@ class FactoryTest extends AbstractTestCase
         $factory->setDefaultDriver('test');
 
         $this->assertSame('test', $factory->getDefaultDriver());
+    }
+
+    public function testUndefinedConfig()
+    {
+        try {
+            $factory = $this->getFactory();
+            $factory->driver('test');
+        } catch (InvalidArgumentException $e) {
+            $this->assertStringStartsWith('Config driver', $e->getMessage());
+        }
+    }
+
+    public function testUndefinedDriver()
+    {
+        try {
+            $config = $this->app->config->get('phone');
+            $config['drivers']['test'] = [];
+
+            $factory = new PhoneFactory($config);
+            $factory->driver('test');
+        } catch (InvalidArgumentException $e) {
+            $this->assertStringStartsWith('Method', $e->getMessage());
+        }
+    }
+
+    public function testMagicCall()
+    {
+        $factory = $this->getFactory();
+        $phone1 = $factory->formatNumber('+14153902337');
+        $phone2 = $factory->driver()->formatNumber('+14153902337');
+
+        $this->assertSame($phone1, $phone2);
     }
 
     protected function getFactory()
