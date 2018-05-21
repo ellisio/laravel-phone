08d31e22b5badb0ac21693a1b8ebd9189c47ceb6
Andrew Ellis <awellis89@gmail.com>
2018-05-21 14:42:01 -0600

    laravel 5.6 support

---
+++ tests/Exceptions/InvalidCountryCodeExceptionTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace EllisIO\Tests\Phone\Exceptions;
+
+use EllisIO\Phone\Exceptions\InvalidCountryCodeException;
+
+class InvalidCountryCodeExceptionTest extends AbstractTestCase
+{
+    public function testInstanceOf()
+    {
+        $e = new InvalidCountryCodeException;
+        $this->assertInstanceOf(InvalidCountryCodeException::class, $e);
+    }
+}
